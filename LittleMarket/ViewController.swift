ba57de45b23f8720df5a4fa0a47a20a5bc84f53c
韩江鹏 <TooT_cn@live.cn>
2016-09-20 11:02:40 +0800

    Initial Commit

---
+++ LittleMarket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LittleMarket
+//
+//  Created by J on 16/9/20.
+//  Copyright © 2016年 J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
